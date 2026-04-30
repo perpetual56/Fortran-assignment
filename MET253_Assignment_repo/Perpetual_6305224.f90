cd3189daa4ea0fd9b359d71cc6c2260c56ab5736
perpetual56 <perpetualafful56@gmail.com>
2026-04-30 12:10:09 -0800

    6305224.f90

--- MET253_Assignment_repo/Perpetual_6305224.f90
+++ MET253_Assignment_repo/Perpetual_6305224.f90
@@ -1,29 +1,25 @@
 PROGRAM Exams_Grades
    IMPLICIT NONE
-   INTGER,DEMINSION=(10):: Scores=(85,62,45,91,38,74,55,88,61,47)
+   INTEGER,DIMENSION(10):: scores=(/85,62,45,91,38,74,55,88,61,47/)
+   INTEGER::i
    CHARACTER(len=1)::grade
-   PRINT*, 'student score remark: '
+   CHARACTER(len=15)::remark
+   PRINT*,"Student Number","Scores","Grade","Remark"
  do i=1,10
-  i(1)=85
-  i(2)=62
-  i(3)=45
-  i(4)=91
-  i(5)=38
-  i(6)=74
-  i(7)=55
-  i(8)=88
-  i(9)=61
-  i(10)=47
-   end do
- IF, ((score>=80).AND.(score<=100))THEN
-   PRINT*,i,score(i),grade, 'DISTINCTION'
-Else IF, ((score>=60).AND.(score<=79))THEN 
-   PRINT*, 'CREDIT'
-Else IF, ((score>=40).AND.(score<=59))THEN
-   PRINT*, 'PASS'
-Else IF, ((score>=0).AND.(score39)THEN
-   PRINT*, 'FAIL'
+ IF,(scores(i)>=80.AND.scores(i)<=100)THEN
+   grade='A'
+   remark='DISTINCTION'
+Else IF, (scores(i)>=60.AND.scores(i)<=79)THEN 
+   grade='B'
+   remark='CREDIT'
+Else IF, (scores(i)>=40.AND.scores(i)<=59)THEN
+   grade='C'
+   remark='PASS'
+Else IF, (scores(i)>=0.AND.scores(i)<=39)THEN
+   grade='F'
+   remark='FAIL'
    END IF
+   PRINT*,i,"  ",scores(i),"  ",grade,"  ",remark
  END DO
  END PROGRAM Exams_Grades 
                                                                                                                        
